5484072c13b6df74e04e41f4b9d83feba84a3172
Kid Kaboom <jt.tirado@gmail.com>
2021-12-27 19:31:09 -0800

    Adding initial files

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+﻿#include <maya\MSimple.h>
+#include <maya\MGlobal.h>
+
+DeclareSimpleCommand(Inbetween, "Justin Tirado", "1.0")
+
+MStatus Inbetween::doIt(const MArgList& args) {
+    MGlobal::displayInfo("Inbetween!");
+    return MS::kSuccess;
+}
